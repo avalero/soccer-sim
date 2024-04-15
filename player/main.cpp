bfb81d42caee96ace550392b7ed3ad80788007bd
Alberto Valero <alberto.valero@bq.com>
2024-04-15 22:54:22 +0200

    working init player

--- player/main.cpp
+++ player/main.cpp
@@ -7,16 +7,30 @@ using namespace std;
 int main()
 {
 
-    MinimalSocket::Port this_socket_port = 15768;
+    MinimalSocket::Port this_socket_port = 5555;
+
+    cout << "Creating a UDP socket" << endl;
+
     MinimalSocket::udp::Udp<true> udp_socket(this_socket_port, MinimalSocket::AddressFamily::IP_V6);
+
+    cout << "Socket created" << endl;
 
     bool success = udp_socket.open();
 
+    if (!success)
+    {
+        cout << "Error opening socket" << endl;
+        return 1;
+    }
+
     // send a message to another udp
-    MinimalSocket::Address other_recipient_udp = MinimalSocket::Address{"192.168.125.85", 15768};
-    udp_socket.sendTo("a message to send", other_recipient_udp);
+    MinimalSocket::Address other_recipient_udp = MinimalSocket::Address{"127.0.0.1", 6000};
+    udp_socket.sendTo("(init MyTeam(version 19))", other_recipient_udp);
+    cout << "Message sent" << endl;
+
     // receive a message from another udp reaching this one
     std::size_t message_max_size = 1000;
+    cout << "Waiting for a message" << endl;
     auto received_message = udp_socket.receive(message_max_size);
     // check the sender address
     MinimalSocket::Address other_sender_udp = received_message->sender;
@@ -24,7 +38,6 @@ int main()
     // resized to the nunber of bytes
     // actually received
     std::string received_message_content = received_message->received_message;
-
     cout << received_message_content << endl;
     return 0;
 }
